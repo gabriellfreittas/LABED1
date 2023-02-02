058adac5a47d8c818c314e1e51c1b0f9ab912014
Gabriel De Freitas Andrade <114424668+gabriellfreittas@users.noreply.github.com>
2023-02-02 11:22:27 -0600

    Add files via upload

---
+++ exerciciolab1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+	int tamanho = 10;
+	int i;
+	int * vetor = (int*) malloc(tamanho*sizeof(int));
+	if (vetor ==NULL){
+		printf("Erro. sem memória");
+		exit(1);
+	}
+	for(i=0; i<tamanho; i++){
+		vetor[i]=i;
+	}
+	
+	//Imprimir vetor
+	for (i=0; i<tamanho; i++){
+		printf("%d\t", vetor[i]);
+	}
+	free(vetor);
+	return 0;
+}
